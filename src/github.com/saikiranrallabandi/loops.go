562c420d591925750aba3db4137c192dd03224a4
saikiranrallabandi <saikiranrallabandi98@gmail.com>
2022-04-21 08:53:07 -0500

    🐎 ci:

--- src/github.com/saikiranrallabandi/loops.go
+++ src/github.com/saikiranrallabandi/loops.go
@@ -4,18 +4,40 @@ import (
 	"fmt"
 )
 
+
 func main() {
+//Loop:
 	    //  for i, j := 0, 0; i < 5; i, j = i+1, j+2 {
 		// 	      fmt.Println(i,j)
 		//  }
-		for i := 0; i < 5; i++ {
-		        fmt.Println(i)
-				if i%2 == 0 {
-					    i /=2
-				} else {
-					    i = 2*i + 1
-				}
+		// for i := 0; i < 5; i++ {
+		//         fmt.Println(i)
+		// 		if i%2 == 0 {
+		// 			    i /=2
+		// 		} else {
+		// 			    i = 2*i + 1
+		// 		}
 
 
+		// }
+		// i :=0
+		// for ;i < 5; {
+		// 	      fmt.Println(i)
+		// 		  i++
+		// }
+		// for i := 1; i <=3; i++ {
+		// 	for j := 1; j <=3; j++ {
+		// 		    fmt.Println( i * j)
+		// 			if i * j >= 3 {
+		// 				   break Loop
+		// 			}
+
+		// 	}
+
+		// }
+		s := []int{1,2,3}
+		fmt.Println(s)
+		for k, v := range s {
+			     fmt.Println(k,v)
 		}
 }
