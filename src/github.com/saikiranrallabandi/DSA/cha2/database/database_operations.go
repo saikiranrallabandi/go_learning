5dcb9c9e8881a0f50039ca23185e3acffdf40461
tvis-techs <pie@tvis.io>
2022-09-29 06:46:37 -0500

    🐞 fix:

--- src/github.com/saikiranrallabandi/DSA/cha2/database/database_operations.go
+++ src/github.com/saikiranrallabandi/DSA/cha2/database/database_operations.go
@@ -1,10 +1,11 @@
 package main
 import (
 	"fmt"
+	"log"
+	"os"
+	"database/util"
 	"database/sql"
      _ "github.com/go-sql-driver/mysql"
-	 "os"
-    "strings"
 )
 
 
@@ -17,21 +18,57 @@ type Customer struct {
 
 //GetConnection method which returns sql.DB
 func GetConnection() (database *sql.DB)  {
-	databaseDriver := "mysql"
-	databaseUser := "root"
-	databasePass := "mauFJcuf5dhRMQrjj"
-	databaseName := "quotes"
-	database, error := sql.Open(databaseDriver, databaseUser+":"+databasePass+"@tcp(127.0.0.1:3306)/"+databaseName)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+        log.Fatal("cannot load config:", err)
+    }
+	fmt.Println(config.DBSource);
+	database, error := sql.Open(config.DBDriver, os.Getenv("DB_SOURCE"))
 	if error != nil {
 		panic(error.Error())
 	}
-
-	defer database.Close()
-
 	return database
 
 }
 
+// Get Customers method returns Customer Array
+
+func GetCustomers() []Customer {
+	var database *sql.DB
+	database = GetConnection()
+
+	var error error
+	var rows *sql.Rows
+	rows, error = database.Query("SELECT * FROM Customer ORDER BY Customerid DESC")
+	if error != nil {
+		panic(error.Error())
+	}
+	var customer Customer
+	customer = Customer{}
+
+
+	var customers []Customer
+	customers = []Customer{}
+	for rows.Next() {
+		var customerId int
+		var customerName string
+		var ssn string
+		error = rows.Scan(&customerId, &customerName, &ssn)
+		if error != nil {
+		panic(error.Error())
+	}
+	customer.CustomerId = customerId
+	customer.CustomerName = customerName
+	customer.SSN = ssn
+	customers = append(customers, customer)
+	}
+     defer database.Close()
+
+	 return customers
+}
+
 func main() {
-	fmt.Println(GetConnection());
+	var customers []Customer
+	customers = GetCustomers()
+	fmt.Println("Customers", customers)
 }
